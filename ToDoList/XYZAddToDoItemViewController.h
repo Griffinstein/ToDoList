9fe0f27e7be8d11ca5b8d160092d150393092721
Griffinstein <eddmccaffrey@gmail.com>
2014-02-05 02:19:17 +1100

    First Objective C Code 2014
    First or many, hurray for me.

---
+++ ToDoList/XYZAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZAddToDoItemViewController.h
+//  ToDoList
+//
+//  Created by Griffin on 5/02/14.
+//  Copyright (c) 2014 Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAddToDoItemViewController : UIViewController
+
+@end
---
+++ ToDoList/XYZToDoList.h
@@ -0,0 +1,15 @@
+//
+//  XYZToDoList.h
+//  ToDoList
+//
+//  Created by Griffin on 5/02/14.
+//  Copyright (c) 2014 Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZToDoList : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+
+@end
